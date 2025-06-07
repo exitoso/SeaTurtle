3b511470606991e8ae89f731777d521c0f1c1598
korotaev.vg <korotaev.vg@yandex.ru>
2025-06-07 09:54:39 +0300

    menu 2

--- core/src/main/java/ru/myitschool/turtle/MenuScreen.java
+++ core/src/main/java/ru/myitschool/turtle/MenuScreen.java
@@ -2,21 +2,26 @@ package ru.myitschool.turtle;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class MenuScreen extends ScreenAdapter {
     private final Stage stage;
     private final MainGame game;
+    private FitViewport viewport;
 
     public MenuScreen(MainGame mainGame) {
         this.game = mainGame;
-        stage = new Stage();
+        viewport = new FitViewport(MainGame.WORLD_WIDTH / 1.5F, MainGame.WORLD_HEIGHT / 1.5F);
+        stage = new Stage(viewport);
         Gdx.input.setInputProcessor(stage);
 
         ImageTextButton.ImageTextButtonStyle textButtonStyle = new ImageTextButton.ImageTextButtonStyle();
@@ -26,22 +31,17 @@ public class MenuScreen extends ScreenAdapter {
         ImageTextButton playButton = new ImageTextButton("Play", textButtonStyle);
         ImageTextButton exitButton = new ImageTextButton("Exit", textButtonStyle);
 
-        playButton.setSize(200, 60);
-        playButton.setPosition(
-            Gdx.graphics.getWidth()/2f - 100,
-            Gdx.graphics.getHeight()/2f + 40
-        );
-
-        exitButton.setSize(200, 60);
-        exitButton.setPosition(
-            Gdx.graphics.getWidth()/2f - 100,
-            Gdx.graphics.getHeight()/2f - 40
-        );
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(playButton).center();
+        table.row().pad(40);
+        table.add(exitButton).center();
 
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new GameScreen(game));
+                dispose();
             }
         });
 
@@ -52,8 +52,7 @@ public class MenuScreen extends ScreenAdapter {
             }
         });
 
-        stage.addActor(playButton);
-        stage.addActor(exitButton);
+        stage.addActor(table);
     }
 
     @Override
@@ -63,5 +62,15 @@ public class MenuScreen extends ScreenAdapter {
 
         stage.act(delta);
         stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 }
